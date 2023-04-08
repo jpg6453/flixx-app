469238a859a893fb414d2377347deda85a912d67
Jon Gill <jonpgill1@gmail.com>
2023-04-08 20:58:16 +0100

    script.js: page router and active link added

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// highlight active link
+const highlightActiveLink = () => {
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+};
+
+//init App
+
+const init = () => {
+  switch (global.currentPage) {
+    case '/':
+    case 'index.html':
+      console.log('Home Page');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('Tv Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+  highlightActiveLink();
+};
+
+document.addEventListener('DOMContentLoaded', init);
