197e16b22777aab64d591b36b6503592cbaf58d8
Jon Gill <jonpgill1@gmail.com>
2023-04-09 16:36:15 +0100

    add spinner and function to fetch TV shows data

--- js/script.js
+++ js/script.js
@@ -39,18 +39,65 @@ const displayPopularMovies = async () => {
     document.querySelector('#popular-movies').appendChild(movieDiv);
   });
 };
+// display TV shows
+
+const displayPopularShows = async () => {
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const showDiv = document.createElement('div');
+    showDiv.classList.add('card');
+    showDiv.innerHTML = `
+    
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `
+                <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `
+            <img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Aired: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+    document.querySelector('#popular-shows').appendChild(showDiv);
+  });
+};
 
 // fetch data from TMBD API
 
 const fetchAPIData = async (endpoint) => {
   const API_KEY = config.API_KEY;
   const API_URL = 'https://api.themoviedb.org/3/';
-
+  showSpinner();
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
   const data = await response.json();
+  hideSpinner();
   return data;
+};
+
+// show / hide spinner
+
+const showSpinner = () => {
+  document.querySelector('.spinner').classList.add('show');
+};
+const hideSpinner = () => {
+  document.querySelector('.spinner').classList.remove('show');
 };
 
 // highlight active link
@@ -72,7 +119,7 @@ const init = () => {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
