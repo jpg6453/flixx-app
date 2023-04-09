e82db107b801907db4c479d51b05ec9018bb2353
Jon Gill <jonpgill1@gmail.com>
2023-04-09 19:35:50 +0100

    add movie slider to index page

--- js/script.js
+++ js/script.js
@@ -244,6 +244,51 @@ const displayBackgroundImage = (type, backgroundPath) => {
   }
 };
 
+// Display movie slider
+
+const displaySlider = async () => {
+  const { results } = await fetchAPIData('movie/now_playing');
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+      </h4>
+          `;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  });
+};
+
+const initSwiper = () => {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInterction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+};
+
 // fetch data from TMBD API
 
 const fetchAPIData = async (endpoint) => {
@@ -288,7 +333,8 @@ const addCommasToNumber = (number) => {
 const init = () => {
   switch (global.currentPage) {
     case '/':
-    case 'index.html':
+    case '/index.html':
+      displaySlider();
       displayPopularMovies();
       break;
     case '/shows.html':
